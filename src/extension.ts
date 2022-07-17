85a0e929f1eebdcbc9fe15dba24263583abb1cf6
denesfilotas <denesfilotas@gmail.com>
2022-07-17 17:01:24 +0200

    Move open example command to separate function

--- src/extension.ts
+++ src/extension.ts
@@ -553,6 +553,43 @@ function showSampleBrowser(viewpath: string) {
 }
 
 /**
+ * Open example in read-only editor of specified command, or launch example browser quickpick menu
+ * @param command command to open example for
+ */
+function openExample(command?: string) {
+    const cpath = path.join(blitzpath, 'help', 'commands') + path.sep;
+    if (command) {
+        const commands2d = readdirSync(cpath + '2d_examples');
+        if (commands2d.includes(command + '.bb')) vscode.workspace.openTextDocument(vscode.Uri.parse('bbdoc:' + path.join(cpath, '2d_examples', command + '.bb'), true)).then((document) => { vscode.window.showTextDocument(document); });
+        else {
+            const commands3d = readdirSync(cpath + '3d_examples');
+            if (commands3d.includes(command + '.bb')) vscode.workspace.openTextDocument(vscode.Uri.parse('bbdoc:' + path.join(cpath, '3d_examples', command + '.bb'), true)).then((document) => { vscode.window.showTextDocument(document); });
+            else vscode.window.showWarningMessage('Could not find example for requested command.');
+        }
+        return;
+    }
+    vscode.window.showQuickPick(['2D command examples', '3D command examples', 'Samples']).then((category) => {
+        switch (category) {
+            case '2D command examples':
+                const commands2d = readdirSync(cpath + '2d_examples');
+                vscode.window.showQuickPick(commands2d).then((val) => {
+                    if (val) vscode.workspace.openTextDocument(vscode.Uri.parse('bbdoc:' + path.join(cpath, '2d_examples', val), true)).then((document) => { vscode.window.showTextDocument(document); });
+                });
+                break;
+            case '3D command examples':
+                const commands3d = readdirSync(cpath + '3d_examples');
+                vscode.window.showQuickPick(commands3d).then((val) => {
+                    if (val) vscode.workspace.openTextDocument(vscode.Uri.parse('bbdoc:' + path.join(cpath, '3d_examples', val), true)).then((document) => { vscode.window.showTextDocument(document); });
+                });
+                break;
+            case 'Samples':
+                let viewpath = path.join(blitzpath, 'samples');
+                showSampleBrowser(viewpath);
+        }
+    });
+}
+
+/**
  * Read the blitz help documents to obtain builtin keywords, and save them in the stubs.bb file
  */
 function generateStubs() {
@@ -698,40 +735,7 @@ export function activate(context: vscode.ExtensionContext) {
             term.sendText('blitzcc "' + vscode.window.activeTextEditor?.document.fileName + '"');
         })
     );
-    context.subscriptions.push(
-        vscode.commands.registerCommand('extension.blitz3d.openExample', (command?: string) => {
-            const cpath = path.join(blitzpath, 'help', 'commands') + path.sep;
-            if (command) {
-                const commands2d = readdirSync(cpath + '2d_examples');
-                if (commands2d.includes(command + '.bb')) vscode.workspace.openTextDocument(vscode.Uri.parse('bbdoc:' + path.join(cpath, '2d_examples', command + '.bb'), true)).then((document) => { vscode.window.showTextDocument(document); });
-                else {
-                    const commands3d = readdirSync(cpath + '3d_examples');
-                    if (commands3d.includes(command + '.bb')) vscode.workspace.openTextDocument(vscode.Uri.parse('bbdoc:' + path.join(cpath, '3d_examples', command + '.bb'), true)).then((document) => { vscode.window.showTextDocument(document); });
-                    else vscode.window.showWarningMessage('Could not find example for requested command.');
-                }
-                return;
-            }
-            vscode.window.showQuickPick(['2D command examples', '3D command examples', 'Samples']).then((category) => {
-                switch (category) {
-                    case '2D command examples':
-                        const commands2d = readdirSync(cpath + '2d_examples');
-                        vscode.window.showQuickPick(commands2d).then((val) => {
-                            if (val) vscode.workspace.openTextDocument(vscode.Uri.parse('bbdoc:' + path.join(cpath, '2d_examples', val), true)).then((document) => { vscode.window.showTextDocument(document); });
-                        });
-                        break;
-                    case '3D command examples':
-                        const commands3d = readdirSync(cpath + '3d_examples');
-                        vscode.window.showQuickPick(commands3d).then((val) => {
-                            if (val) vscode.workspace.openTextDocument(vscode.Uri.parse('bbdoc:' + path.join(cpath, '3d_examples', val), true)).then((document) => { vscode.window.showTextDocument(document); });
-                        });
-                        break;
-                    case 'Samples':
-                        let viewpath = path.join(blitzpath, 'samples');
-                        showSampleBrowser(viewpath);
-                }
-            });
-        })
-    );
+    context.subscriptions.push(vscode.commands.registerCommand('extension.blitz3d.openExample', openExample));
     context.subscriptions.push(vscode.commands.registerCommand('extension.blitz3d.generatestubs', generateStubs));
 
     // Providers
